34a35ad973350772f8c602cab6192ef3cb3e3d37
ElenaObukhova1984 <e.obukhova84@gmail.com>
2022-10-01 22:16:09 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const bodyRef = document.querySelector('body');
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
 
+stopBtnRef.disabled = true;
+let timerId = null;
+
+startBtnRef.addEventListener('click', handleStart);
+stopBtnRef.addEventListener('click', handleStop);
+
+
+function handleStart() {
+    startBtnRef.disabled = true;
+    stopBtnRef.disabled = false;
+    timerId = setInterval(() => {
+        bodyRef.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+};
+
+function handleStop() {
+  clearTimeout(timerId);
+  startBtnRef.disabled = false;
+  stopBtnRef.disabled = true;
+
+};
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
