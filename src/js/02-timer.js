3d5354d01c689a983f652ccca3d5f7230595ec7b
ElenaObukhova1984 <e.obukhova84@gmail.com>
2022-10-01 22:24:07 +0300

    task2

---
+++ src/js/02-timer.js
@@ -0,0 +1,29 @@
+// Описан в документации
+import flatpickr from "flatpickr";
+// Дополнительный импорт стилей
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+
+
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
