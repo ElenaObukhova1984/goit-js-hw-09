a8538878178a6565c019ecfd3b585ef346199c6b
ElenaObukhova1984 <e.obukhova84@gmail.com>
2022-10-02 21:51:30 +0300

    task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,13 +1,66 @@
-// Описан в документации
 import flatpickr from "flatpickr";
-// Дополнительный импорт стилей
 import "flatpickr/dist/flatpickr.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+	
 
+const inputRef = document.querySelector('#datetime-picker');
+const startBtnRef = document.querySelector('[data-start]');
 
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
 
+startBtnRef.disabled = true;
+	
+const options = {
+	  enableTime: true,
+	  time_24hr: true,
+	  defaultDate: new Date(),
+	  minuteIncrement: 1,
+	  onClose(selectedDates) {
+	    if (selectedDates[0].getTime() < Date.now()) {
+	    Notify.failure("Please choose a date in the future");
+	    return;
+	    }
+		startBtnRef.disabled = false;
+	    return;
+	  }
+	};
+	
+	const timerDate = flatpickr(inputRef, options);
+	
+	startBtnRef.addEventListener('click', handleTime)
+	
+	function handleTime() {
+	  if (timerDate.selectedDates[0].getTime() > Date.now()) {
+	    inputRef.disabled = true;
+		startBtnRef.disabled = true;
+	    	
+	    const timerId = setInterval(() => {
+		const delta = timerDate.selectedDates[0].getTime() - Date.now();
+	        
+	    timeUpdate(convertMs(delta));
+		stop(delta, timerId);
+		}, 1000
+	    )
+	  };
+	};
+	
+	function stop(countTime, timerId) {
+	  if (countTime <= 1000) {
+	    clearInterval(timerId);
+	   		  }
+	}
 
-
+function timeUpdate({ days, hours, minutes, seconds }) {
+  daysRef.textContent = addLeadingZero(days);
+  hoursRef.textContent = addLeadingZero(hours);
+  minutesRef.textContent = addLeadingZero(minutes);
+  secondsRef.textContent = addLeadingZero(seconds);
+  }
+  
+const addLeadingZero = value => value.toString().padStart(2, 0);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
