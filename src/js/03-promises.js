cabe52d7b9ac6327e797bd29737429f3cb5ff0f2
ElenaObukhova1984 <e.obukhova84@gmail.com>
2022-10-03 11:50:02 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+		
+	const formRef= document.querySelector('.form');
+	formRef.addEventListener('submit', handleSubmit);
+	
+	
+	function createPromise(position, delay) {
+	  const shouldResolve = Math.random() > 0.3;
+	  return new Promise((resolve, reject) => {
+	    setTimeout(() => {
+	      if (shouldResolve) {
+	        resolve({ position, delay });
+	      } else {
+	        reject({ position, delay });
+	      }
+	    }, delay);
+	  });
+	}
+	 
+	function handleSubmit(event) {
+	  event.preventDefault();
+	
+	const {
+    elements: { delay, step, amount }
+  } = event.currentTarget;
+
+	
+	  let numberDelay = Number(delay.value);
+	  const numberStep = Number(step.value);
+	  const numberAmount = Number(amount.value);
+	
+	
+	  for (let i = 1; i <= numberAmount; i += 1) {
+	    createPromise(i, numberDelay)
+	      .then(({ position, delay }) => {
+	        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+	      })
+	      .catch(({ position, delay }) => {
+	        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+	      });
+	    numberDelay += numberStep;
+	  }
+	}
+
